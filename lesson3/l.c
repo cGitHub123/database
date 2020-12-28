a539db9f1d95c13a611278470464647a6a8852c1
caibin <caibin@58.com>
2020-12-28 17:22:35 +0800

    修改

--- lesson3/l.c
+++ lesson3/l.c
@@ -1,7 +1,11 @@
+//
+// Created by Administrator on 2020/12/27.
+//
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 typedef struct {
     char *buffer;
@@ -38,15 +42,21 @@ void close_input_buffer(InputBuffer *input_buffer) {
     free(input_buffer);
 }
 
-// 系统命令枚举.if
+// 系统命令枚举.
 typedef enum {
     MEAT_COMMAND_SUCCESS,
     MEAT_COMMAND_ERROR
 } MetaCommandResult;
 
+// 验证表是否满了的枚举.
+typedef enum {
+    EXECUTE_SUCCESS, EXECUTE_TABLE_FULL
+} ExecuteResult;
+
 // 普通命令枚举.
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_ERROR
 } PrepareResult;
 
@@ -55,16 +65,92 @@ typedef enum {
     STATEMENT_SELECT
 } StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 typedef struct {
     StatementType type;
+    Row row_to_insert;
 } Statement;
 
-MetaCommandResult doMetaCommand(InputBuffer *input_buffer) {
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+// 定义数据大小.
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USER_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+
+// 定义对应的偏移量.
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + USER_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USER_SIZE;
+
+// 每条数据的总大小
+const uint32_t ROW_SIZE = ID_SIZE + USER_SIZE + EMAIL_SIZE;
+
+
+// 序列化方法.
+void serialize_row(Row *source, void* destination) {
+    // C和C++使用的内存拷贝函数.
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USER_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+// 反序列化方法.
+void derialize_row(void *source, Row* destination) {
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USER_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+// 限制每个表的总页数.
+#define TABLE_MAX_PAGES 100
+
+// 每个页的页大小.
+const uint32_t PAGE_SIZE = 4096;
+
+// 平均每页的行数,行不要跨页存储.
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+
+// 表中总的行数.
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+    uint32_t num_rows;
+    void *pages[TABLE_MAX_PAGES];
+} Table;
+
+
+// 读取特定的一行.
+void *row_slot(Table *table, uint32_t row_num) {
+    // 根据行号获取它在哪一页.
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+
+    void *page = table->pages[page_num];
+
+    if (page == NULL) {
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+
+    uint32_t byte_offset = row_offset % ROW_SIZE;
+
+    return page + byte_offset;
+}
+
+MetaCommandResult doMetaCommand(InputBuffer *input_buffer, Table *table) {
     // strcmp的意思是比较两个字符串的意思.
-    //printf(input_buffer->buffer);
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
         close_input_buffer(input_buffer);
-        printf(12345);
+        free(table);
         exit(EXIT_SUCCESS);
     } else {
         return MEAT_COMMAND_ERROR;
@@ -72,8 +158,15 @@ MetaCommandResult doMetaCommand(InputBuffer *input_buffer) {
 }
 
 PrepareResult doPrepareResult(InputBuffer *input_buffer, Statement *statement) {
-    if (strcmp(input_buffer->buffer, "insert") == 0) {
+    // 插入校验.
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
         statement->type = STATEMENT_INSERT;
+        // sscanf就是一个格式化的输入.
+        int arg_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+                                  statement->row_to_insert.username, statement->row_to_insert.email);
+        if (arg_assigned < 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
@@ -83,25 +176,68 @@ PrepareResult doPrepareResult(InputBuffer *input_buffer, Statement *statement) {
     return PREPARE_ERROR;
 }
 
-void excute_statement(Statement *statement) {
+ExecuteResult excute_insert(Statement *statement, Table *table) {
+    // 如果表已经满了,就返回错误.
+    if (table->num_rows >= TABLE_MAX_ROWS) {
+        return EXECUTE_TABLE_FULL;
+    }
+    // &取地址符.
+    Row *row_to_insert = &(statement->row_to_insert);
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+    return EXECUTE_SUCCESS;
+}
+
+void print_row(Row *row) {
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+ExecuteResult excute_select(Statement *statement, Table *table) {
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; i++) {
+        derialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult excute_statement(Statement *statement, Table *table) {
     switch (statement->type) {
         case (STATEMENT_INSERT):
-            printf("insert\n");
-            break;
+            return excute_insert(statement, table);
         case (STATEMENT_SELECT):
-            printf("select\n");
-            break;
+            return excute_select(statement, table);
     }
 }
 
+Table *new_table() {
+    // malloc分配所需的内存空间，并返回一个指向它的指针.
+    // sizeof分配的是字节大小.
+    Table *table = malloc(sizeof(Table));
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+void free_table(Table *table) {
+    // table里面有个指针所以要循环释放.
+    for (int i = 0; table->pages[i]; i++) {
+        free(table->pages[i]);
+    }
+    free(table);
+}
+
 int main(int argc, char *argv[]) {
+    Table *table = new_table();
     InputBuffer *input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
         read_input(input_buffer);
         // 系统命令.
         if (input_buffer->buffer[0] == '.') {
-            switch (doMetaCommand(input_buffer)) {
+            switch (doMetaCommand(input_buffer, table)) {
                 case (MEAT_COMMAND_SUCCESS):
                     continue;
                 case (MEAT_COMMAND_ERROR) :
@@ -109,18 +245,29 @@ int main(int argc, char *argv[]) {
                     continue;
             }
         }
-        // 检查语法
+
+        // 检查语法.
         Statement statement;
         switch (doPrepareResult(input_buffer, &statement)) {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_SYNTAX_ERROR):
+                printf("syntax error\n");
+                continue;
             case (PREPARE_ERROR):
                 printf("unrecognized\n");
                 continue;
         }
 
-        excute_statement(&statement);
-        printf("excute!.\n");
+        // 执行SQL.
+        switch (excute_statement(&statement, table)) {
+            case (EXECUTE_SUCCESS):
+                printf("Executed.\n");
+                break;
+            case (EXECUTE_TABLE_FULL):
+                printf("full.\n");
+                break;
+        }
     }
 }
 
