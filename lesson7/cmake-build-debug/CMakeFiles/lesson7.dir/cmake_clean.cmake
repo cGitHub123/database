9df0c8de14df35e9070db2295543404bd216ea68
caibin <caibin@58.com>
2020-12-31 17:59:42 +0800

    修改

---
+++ lesson7/cmake-build-debug/CMakeFiles/lesson7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lesson7.dir/l.c.o"
+  "lesson7"
+  "lesson7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lesson7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
